7df65f8615fa54fae21d24bad46d8926f846adb3
Antonio Luis <luissombra@uern.br>
2014-10-24 08:18:39 -0300

    modificação(*):adicionado a tarefa bump para fazer o incremento da versão

--- gruntfile.js
+++ gruntfile.js
@@ -136,7 +136,23 @@ module.exports = function(grunt) {
 			unit: {
 				configFile: 'karma.conf.js'
 			}
-		}
+		},
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'upstream',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false
+            }
+        }
 	});
 
 	// Load NPM tasks
